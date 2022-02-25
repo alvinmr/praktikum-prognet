5b99d5189d69b8ffa26c0d1571a4d2ca64fd9b01
gussatya88 <99333065+gussatya88@users.noreply.github.com>
2022-02-25 18:59:10 +0800

    migrasi

---
+++ database/migrations/2022_02_25_090331_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('timeout');
+            $table->string('address');
+            $table->string('regency');
+            $table->string('province');
+            $table->bigInteger('total');
+            $table->bigInteger('shipping_cost');
+            $table->bigInteger('sub_total');
+            $table->foreignId('user_id');
+            $table->foreignId('courier_id');
+            $table->string('proof_of_payment');
+            $table->enum('status', ['Berhasil','Tidak Berhasil','Dibatalkan','Belum Terbayar']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
