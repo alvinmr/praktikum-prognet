b6cb814429d067646f312bf42686989bb859a1bd
alvinmr <maulanaalvin1@gmail.com>
2022-03-11 03:37:07 +0800

    set up model

--- app/Models/ProductCategoryDetail.php
+++ app/Models/ProductCategoryDetail.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProductCategoryDetail extends Model
 {
     use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionDetail extends Model
 {
     use HasFactory;
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
